add9b1a65184dfde03372611ad8303aaf95ea643
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-03-12 20:42:20 +0530

    Added buttons on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'pushable_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,14 +13,39 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      margin: const EdgeInsets.all(24),
-      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-      child: Column(
-        children: [
-          _header(context),
-        ],
-      ),
+        body: Column(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(24),
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Column(
+            children: [
+              _header(context),
+            ],
+          ),
+        ),
+        SizedBox(
+            width: 200,
+            child: PushableButton(
+              child: Text(
+                'Chat Bot',
+                style: (TextStyle(
+                  color: Color.fromRGBO(101, 93, 187, 1),
+                  fontSize: 20,
+                )),
+              ),
+              height: 70,
+              elevation: 5,
+              hslColor: HSLColor.fromColor(Color.fromRGBO(185, 243, 228, 1)),
+              shadow: BoxShadow(
+                color: Color.fromRGBO(185, 243, 228, 0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 2),
+              ),
+              onPressed: () {},
+            ))
+      ],
     ));
   }
 
@@ -34,7 +60,7 @@ class _HomeScreenState extends State<HomeScreen> {
               fontWeight: FontWeight.w500,
             ))),
         Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(2.0),
           child: SizedBox(
             height: 200,
             child: Image.asset(
