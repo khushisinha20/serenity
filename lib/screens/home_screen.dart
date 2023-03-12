7969a254596a6ff6f903a6bbaf8d4e0d72abd190
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-03-12 18:48:51 +0530

    Added a Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,56 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+          body: Container(
+        margin: const EdgeInsets.all(24),
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          children: [
+            _header(context),
+          ],
+        ),
+      )),
+    );
+  }
+
+  _header(context) {
+    return Column(
+      children: [
+        Text("Welcome, Name",
+            style: GoogleFonts.merriweatherSans(
+                textStyle: const TextStyle(
+              fontSize: 20,
+              color: Color.fromRGBO(101, 93, 187, 1),
+              fontWeight: FontWeight.w500,
+            ))),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: 250,
+            child: Image.asset(
+              "assets/home_page_image.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 5, 0, 10),
+          child: Text("Sorting through negative emotions?",
+              style: GoogleFonts.merriweatherSans(
+                  textStyle: const TextStyle(
+                fontSize: 20,
+                color: Color.fromRGBO(101, 93, 187, 1),
+              ))),
+        ),
+        Text("Start your journey with us.",
+            style: GoogleFonts.merriweatherSans(
+                textStyle: const TextStyle(
+              fontSize: 15,
+              color: Color.fromRGBO(101, 93, 187, 1),
+            ))),
+      ],
+    );
   }
 }
